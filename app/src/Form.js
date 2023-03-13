1ea5654e6884dc0e393d2b90b1abe43a283210e0
elwalid <wazardo99@gmail.com>
2023-03-14 01:54:55 +0300

    the main task is done

--- app/src/Form.js
+++ app/src/Form.js
@@ -5,35 +5,69 @@ import { songsdata } from "./audio";
 
 const Form = () => {
   const [url, setUrl] = useState("");
+  const [error, setError] = useState(false);
+  const [flag, setFlag] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (songsdata.some((element) => element.url === url)) {
+      setError(false);
+      console.log("sx");
+      setFlag(true);
+    } else {
+      console.log("cdk");
+      setError(true);
+    }
+  };
+
+  function style(error) {
+    if (error) {
+      return {
+        backgroundColor: "rgba(255, 0, 0, 0.5)",
+      };
+    }
+  }
 
   const handleChange = (event) => {
     setUrl(event.target.value);
   };
 
-  const [flag, setFlag] = useState(false);
-
-  const handleUrl = (url, arr) => {
-    let res = arr.some((element) => element === url);
-    if (!res) {
+  const toggleButton = (event) => {
+    if (songsdata.some((element) => element.url === url)) {
       setFlag(true);
     }
   };
 
   return (
-    <div>
-      <form>
-        <input
-          value={url}
-          onChange={handleChange}
-          className="link_text"
-          type="text"
-          placeholder="https://"
-        />
-        <button onClick={handleUrl(url, songsdata)} className="btn_form">
-          Click
-        </button>
-      </form>
-    </div>
+    <>
+      {flag ? (
+        <AudioPlayer />
+      ) : (
+        <div>
+          <form className="form" onSubmit={handleSubmit}>
+            <div>
+              <input
+                onChange={handleChange}
+                className="link_text"
+                type="text"
+                placeholder="https://"
+                style={style(error)} // ADDED
+                value={url}
+              />
+              {error && (
+                <p style={{ color: "#f8d231" }} role="alert">
+                  Error message here
+                </p>
+              )}
+            </div>
+
+            <button onClick={toggleButton} className="btn_form">
+              Click
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
